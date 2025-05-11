5752b737ca22f53cd9bbadeec6b47d9a36172c52
23-faisal <a.faisal23r@gmail.com>
2025-05-11 07:48:19 +0600

    create status code human readable

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,4 +1,5 @@
 import { ErrorRequestHandler } from "express";
+import { INTERNAL_SERVER_ERROR } from "../constants/http";
 
 const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
   console.error(`Error at ${req.method} ${req.path}:`, error);
@@ -7,7 +8,7 @@ const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
 
   const message = error.message || "Internal server error";
 
-  res.status(status).json({
+  res.status(INTERNAL_SERVER_ERROR).json({
     success: false,
     message,
   });
